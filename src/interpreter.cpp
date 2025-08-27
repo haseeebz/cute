c897ff397f96e09da281bd2e95c4b4f1f727e784
haseeb <haseeeb8j@gmail.com>
2025-08-27 21:58:50 +0500

    Basic interpretation of expressions

--- src/interpreter.cpp
+++ src/interpreter.cpp
@@ -6,23 +6,13 @@
 
 void Interpreter::run()
 {
-	ContainerVec* cons = tokenizer.tokenize("12121 + 59 / 6");
+	ContainerVec* cons = tokenizer.tokenize("2 + 2 + 2 * 2");
 	ContainerVec* parsed_cons = parser.parse(cons);
-	print_containers(parsed_cons);
+	
+	evaluator.evaluate(parsed_cons);
+	int result = evaluator.yield();
+	std::cout << "Result: " << result << std::endl;
+
 	delete cons;
 	delete parsed_cons;
-
-	ContainerVec containers;
-	containers.push_back(Container(3));
-	containers.push_back(Container(4));
-	containers.push_back(Container(BinaryOpType::Mul));
-	containers.push_back(Container(6));
-	containers.push_back(Container(BinaryOpType::Div));
-	containers.push_back(Container(2));
-	containers.push_back(Container(BinaryOpType::Sub));
-
-	evaluator.evaluate(&containers);
-	int result = evaluator.yield();
-
-	std::cout << "Result: " << result << std::endl;
 }
