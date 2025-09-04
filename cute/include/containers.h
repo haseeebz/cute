041c8dd290f9e3c110bd2323392e20f5f6f88c89
haseeb <haseeeb8j@gmail.com>
2025-09-04 22:12:36 +0500

    Added Container Stack defintions

--- cute/include/containers.h
+++ cute/include/containers.h
@@ -34,3 +34,17 @@ void printContainer(Container* con, bool endline);
 Container newIntContainer(int i);
 Container newBinaryOpContainer(BinaryOpType op);
 
+
+typedef struct {
+	Container* cons;
+	int size;
+	int capacity;
+} ContainerStack;
+
+ContainerStack* newContainerStack(int capacity);
+Container ContainerStack_resize(ContainerStack* stack);
+void ContainerStack_push(ContainerStack* stack, Container con);
+Container ContainerStack_pop(ContainerStack* stack);
+Container ContainerStack_peek(ContainerStack* stack);
+void delContainerStack(ContainerStack* stack);
+void printContainerStack(ContainerStack* stack);
--- cute/src/containers.c
+++ cute/src/containers.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 #include "../include/containers.h"
 
 
@@ -36,3 +37,35 @@ Container newBinaryOpContainer(BinaryOpType op)
 	con.value.bop = op;
 	return con;
 }
+
+
+ContainerStack* newContainerStack(int capacity)
+{
+	ContainerStack* stack = malloc(sizeof(ContainerStack));
+	stack->capacity = capacity;
+	stack->size = 0;
+
+	stack->cons = malloc(sizeof(Container)*capacity);
+
+	return stack;
+}
+
+
+Container ContainerStack_resize(ContainerStack* stack);
+
+
+
+
+void ContainerStack_push(ContainerStack* stack, Container con)
+{
+	stack->cons[stack->size] = con;
+	stack->size++;
+}
+
+
+
+
+Container ContainerStack_pop(ContainerStack* stack);
+Container ContainerStack_peek(ContainerStack* stack);
+void delContainerStack(ContainerStack* stack);
+void printContainerStack(ContainerStack* stack);
