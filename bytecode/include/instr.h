eff6f0dfb70980855277dd7a925ec5befea5a52f
haseeb <haseeeb8j@gmail.com>
2025-10-09 16:30:18 +0500

    Added byteloader headers

---
+++ bytecode/include/instr.h
@@ -0,0 +1,36 @@
+#pragma once 
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#include <stdint.h>
+
+
+typedef uint8_t InstrByte;
+
+typedef enum
+{
+	
+	instrEXIT       = 0x00,
+
+	instrLOADi      = 0x10, // Load an integar from a given stack slot
+	instrSTORi		= 0x11,	// Store an integar to a given address
+	instrLCONi      = 0x12, // Load an integar from the constant pool
+
+	instrADDi       = 0xA1,
+	instrSUBi       = 0xA2,
+	instrMULi       = 0xA3,
+	instrDIVi       = 0xA4,
+	
+	instrOUT        = 0x88, // Temporary Instruction
+
+} Instruction;
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
---
+++ bytecode/include/loader.h
@@ -0,0 +1,50 @@
+#pragma once 
+#include "instr.h"
+#include <stdint.h>
+#include <stdlib.h>
+
+#ifdef  __cplusplus 
+extern "C" {
+#endif
+
+typedef struct
+{
+	enum {charT, intT, floatT} type;
+	union {int32_t i; double d;} val;
+} ConstantValue;
+
+
+typedef struct
+{
+	int32_t magic;
+	int32_t constant_pool_index;
+	int32_t instructions_index;
+} ProgramHeader;
+
+
+typedef struct 
+{
+	ProgramHeader header;
+
+	ConstantValue* constant_pool;
+	size_t const_count;
+
+	Instruction* instrs;
+
+} ProgramContext;
+
+
+ProgramContext* ProgramContext_new();
+void ProgramContext_del(ProgramContext* context);
+
+void ProgramContext_addInstruction(ProgramContext* context, Instruction instr);
+// Appends an instruction to the instructions field
+
+int ProgramContext_addConstant(ProgramContext* context, ConstantValue constant);
+// Adds a constant to the pool and returns its index for later referencing.
+
+
+
+#ifdef  __cplusplus 
+}
+#endif
