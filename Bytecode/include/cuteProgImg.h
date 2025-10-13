cb73ccfae79bc6a7046914a63a265604e7893bb5
haseeb <haseeeb8j@gmail.com>
2025-10-13 16:45:14 +0500

    Rewriting ProgramSource to ProgramImage

---
+++ Bytecode/include/cuteProgImg.h
@@ -0,0 +1,20 @@
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "cuteInstr.h"
+
+#pragma once 
+
+#define MAGIC 0x10211225
+
+
+#ifdef  __cplusplus 
+extern "C" {
+#endif
+
+
+#ifdef  __cplusplus 
+}
+#endif
--- Cute/main.c
+++ Cute/main.c
@@ -1,22 +1,6 @@
-#include "cuteByte.h"
     
 int main()
 {
-	ProgramSource* program = ProgramSource_new();
 	
-
-	ProgramSource_addInstruction(program, instrLOADi);
-	ProgramSource_addInstruction(program, instrLOADi);
-	ProgramSource_addInstruction(program, instrSTORi);
-	ProgramSource_addInstruction(program, instrLCONi);
-	ProgramSource_addInstruction(program, instrEXIT);
-	
-	ProgramSource_writeToFile(program, "file.bin");
-
-	ProgramSource_loadFromFile(program, "file.bin");
-	ProgramSource_print(program);
-
-	ProgramSource_del(program);
-
 	return 0;
 }
