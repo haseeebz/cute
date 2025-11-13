eccbb34ff10cfcfad6b31d3aef81d50c2b9f611f
Haseeb <haseeeb8j@gmail.com>
2025-11-13 23:32:20 +0500

    main fix

--- Cute/main.cpp
+++ Cute/main.cpp
@@ -1,17 +1,8 @@
 #include "include/CuteCore.hpp"
-#include <iostream>
 
 
 int main(int argc, char* argv[])
 {
-	int counter = 0;
-	
-	while (true)
-	{
-		counter++;
-		if (counter > 1000000000) {return 0;}
-	}
-
 	CuteCore core;
 	core.run(argc, argv);
 }
