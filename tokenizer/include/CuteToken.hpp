a691bc5d6dfe1cef0cf15ae828c999543d56a18f
Haseeb <haseeeb8j@gmail.com>
2025-11-27 18:00:00 +0500

    added more helper functions in token stream for easier parsing

--- tokenizer/include/CuteToken.hpp
+++ tokenizer/include/CuteToken.hpp
@@ -25,25 +25,48 @@ struct Token
 };
 
 
-struct TokenStream
+class TokenStream
 {
 	std::string srcStr;
 
 	std::vector<Token> tokens;
 	uint curr_token;
 
+	public:
+
 	TokenStream() {};
 	TokenStream(std::string src);
 
 	void add(Token token);
-	Token next();
-	Token peek();
-	void backtrack(uint i = 1);
-	void gotoIndex(uint i);
+	std::string toString();
+	
 
+	Token next(); // gives you the current token and increments the counter
+	Token peek(); // gives you the current token but does'nt increment
+	uint currentIndex(); // gives you the current index duh
+	void backtrack(uint i = 1); // backtracks the index by the given amount. 
+	void gotoIndex(uint i);	// goes to the given index... duh...
+
+
+	// viewing token
 	std::string viewToken(Token* token);
 	char viewSymToken(Token* token);
-	std::string toString();
+	int64_t viewIntToken(Token* token);
+	double viewFloatToken(Token* token);
+
+
+	// all of these functions try to get the specified type. 
+	// if type found, it is written to the pointer, true is returned and the counter is increased
+	// if not, it returns false, the pointer and the counter is'nt modified 
+	bool getInt(int64_t* i); 
+	bool getFloat(double* d);
+	bool getWord(std::string* str);
+	bool getSym(char* sym);
+
+	// returns true if the current token is word AND matches the given string
+	bool getKeyword(std::string keyword);
+	bool getKeySym(char sym);
+	
 };
 
 
--- tokenizer/token.cpp
+++ tokenizer/token.cpp
@@ -18,62 +18,6 @@ void TokenStream::add(Token token)
 }
 
 
-Token TokenStream::next()
-{
-	if (this->curr_token >= this->tokens.size())
-	{
-		return Token(TokenType::tokenEOF, 0,0);
-	}
-
-	return this->tokens[this->curr_token++];
-}
-
-
-Token TokenStream::peek()
-{
-	if (this->curr_token >= this->tokens.size())
-	{
-		return Token(TokenType::tokenEOF, 0,0);
-	}
-
-	return this->tokens[this->curr_token];
-}
-
-
-void TokenStream::backtrack(uint i)
-{
-	if (this->curr_token <= 0)
-	{
-		this->curr_token = 0;
-		return;
-	}
-	this->curr_token -= i;
-}
-
-void TokenStream::gotoIndex(uint i)
-{
-	if (i >= this->tokens.size())
-	{
-		this->curr_token = this->tokens.size() - 1;
-		return;
-	}
-	this->curr_token = i;
-}
-
-
-std::string TokenStream::viewToken(Token* token)
-{
-	std::string str(this->srcStr.substr(token->start, token->end - token->start + 1));
-	return str;
-}
-
-
-char TokenStream::viewSymToken(Token* token)
-{
-	return this->srcStr[token->start];
-}
-
-
 std::string TokenStream::toString()
 {
 	Token* tok;
@@ -104,3 +48,175 @@ std::string TokenStream::toString()
 	str.append("\n");
 	return str;
 }
+
+
+Token TokenStream::next()
+{
+	if (this->curr_token >= this->tokens.size())
+	{
+		return Token(TokenType::tokenEOF, 0,0);
+	}
+
+	return this->tokens[this->curr_token++];
+}
+
+Token TokenStream::peek()
+{
+	if (this->curr_token >= this->tokens.size())
+	{
+		return Token(TokenType::tokenEOF, 0,0);
+	}
+
+	return this->tokens[this->curr_token];
+}
+
+
+uint TokenStream::currentIndex()
+{
+	return this->curr_token;
+}
+
+
+void TokenStream::backtrack(uint i)
+{
+	if (this->curr_token <= 0)
+	{
+		this->curr_token = 0;
+		return;
+	}
+	this->curr_token -= i;
+}
+
+
+void TokenStream::gotoIndex(uint i)
+{
+	if (i >= this->tokens.size())
+	{
+		this->curr_token = this->tokens.size() - 1;
+		return;
+	}
+	this->curr_token = i;
+}
+
+
+std::string TokenStream::viewToken(Token* token)
+{
+	uint len = token->end - token->start;
+
+	return this->srcStr.substr(token->start, len);
+}
+
+
+char TokenStream::viewSymToken(Token* token)
+{
+	return this->srcStr[token->start];
+}
+
+
+int64_t TokenStream::viewIntToken(Token* token)
+{
+	uint len = token->end - token->start;
+
+	return std::stol(this->srcStr.substr(token->start, len));
+}
+
+
+double TokenStream::viewFloatToken(Token* token)
+{
+	uint len = token->end - token->start;
+
+	return std::stod(this->srcStr.substr(token->start, len));
+}
+
+
+bool TokenStream::getInt(int64_t* i)
+{
+	Token tok = this->next();
+
+	if (tok.type != TokenType::tokenInt)
+	{
+		this->backtrack();
+		return false;
+	}
+
+	*i = this->viewIntToken(&tok);
+	return true;
+}
+
+
+bool TokenStream::getFloat(double* d)
+{
+	Token tok = this->next();
+
+	if (tok.type != TokenType::tokenFloat)
+	{
+		this->backtrack();
+		return false;
+	}
+
+	*d = this->viewFloatToken(&tok);
+	return true;
+}
+
+
+bool TokenStream::getWord(std::string* str)
+{
+	Token tok = this->next();
+
+	if (tok.type != TokenType::tokenWord)
+	{
+		this->backtrack();
+		return false;
+	}
+
+	*str = this->viewToken(&tok);
+	return true;
+}
+
+
+bool TokenStream::getSym(char* sym)
+{
+	Token tok = this->next();
+
+	if (tok.type != TokenType::tokenSymbol)
+	{
+		this->backtrack();
+		return false;
+	}
+
+	*sym = this->viewSymToken(&tok);
+	return true;
+}
+
+
+bool TokenStream::getKeyword(std::string keyword)
+{
+	std::string str;
+
+	if (this->getWord(&str))
+	{
+		if (str == keyword)
+		{
+			return true;
+		}
+	}
+
+	return false;
+}
+
+
+bool TokenStream::getKeySym(char sym)
+{
+	char c;
+
+	if (this->getSym(&c))
+	{
+		if (sym == c)
+		{
+			return true;
+		}
+	}
+
+	return false;
+}
+
