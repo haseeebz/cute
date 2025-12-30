d57c8faae20bcf962cd33bb78710435a215dcfc4
Haseeb <haseeeb8j@gmail.com>
2025-12-30 21:49:02 +0500

    added CuteLib

---
+++ Cute/include/CuteLib.hpp
@@ -0,0 +1,13 @@
+#include <stdint.h>
+
+#pragma once
+
+typedef int32_t  I32;
+typedef int64_t  I64;
+typedef uint32_t U32;
+typedef uint64_t U64;
+typedef float    F32;
+typedef double   F64;
+
+typedef bool     Bool;
+
