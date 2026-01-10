04c65c72f739486ef934d4216f82067dcb2e1535
Haseeb <haseeeb8j@gmail.com>
2026-01-10 22:03:26 +0500

    formatted nicely

--- Cute/include/CuteArray.h
+++ Cute/include/CuteArray.h
@@ -1,24 +1,31 @@
 
 /*
-Template file for standard Cute Array.
+Template file for standard implementation of CuteArray
 */
 
 
-#define CUTE_ARRAY_DEF(NAME, TYPE, LEN) \
-\
-\
-typedef TYPE NAME##_item; \
-\
-typedef struct { \
-	TYPE items[LEN]; \
-} NAME; \
-\
-\
-int NAME##_get(NAME* array, size_t index, TYPE* item) { \
-	if (index >= LEN || index < 0) {return 0;} \
-	else {*item = array->items[index]; return 1;} \
-} \
-int NAME##_set(NAME* array, size_t index, TYPE* item) { \
-	if (index >= LEN || index < 0) {return 0;} \
-	else {array->items[index] = *item; return 1;} \
-} 
+#define CUTE_ARRAY_DEF(NAME, TYPE, SIZE)                                   \
+                                                                           \
+typedef TYPE NAME##_item;                                                  \
+                                                                           \
+typedef struct {                                                           \
+    TYPE items[SIZE];                                                      \
+} NAME;                                                                    \
+                                                                           \
+int NAME##_get(NAME* array, size_t index, TYPE* item) {                    \
+    if (index >= SIZE) {                                                   \
+        return 0;                                                          \
+    } else {                                                               \
+        *item = array->items[index];                                       \
+        return 1;                                                          \
+    }                                                                      \
+}                                                                          \
+                                                                           \
+int NAME##_set(NAME* array, size_t index, TYPE* item) {                    \
+    if (index >= SIZE) {                                                   \
+        return 0;                                                          \
+    } else {                                                               \
+        array->items[index] = *item;                                       \
+        return 1;                                                          \
+    }                                                                      \
+}
