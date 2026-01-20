80bdfc446da816021eb5333c58fc1b3595f24aba
haseeb <haseeeb8j@gmail.com>
2026-01-20 21:08:31 +0500

    added primitive types

--- Cute/include/CuteTypes.h
+++ Cute/include/CuteTypes.h
@@ -2,7 +2,12 @@
 #pragma once
 
 #include <stdint.h>
-#include <sys/types.h>
+
+#define CREATE_FUNC(name, op, typeA, typeB, returnT, returnTConstructor) \
+inline returnT                                   \
+##name(typeA a, typeB b) {                       \
+    return returnTConstructor(a.val op b.val);   \
+}
 
 
 typedef struct
@@ -10,15 +15,24 @@ typedef struct
     int64_t val;
 } cute_Int;
 
+
+typedef struct
+{
+    uint64_t val;
+} cute_UInt;
+
+
 typedef struct 
 {
     double val;
 } cute_Float;
 
+
 typedef struct
 {
-    u_int8_t val;
+    uint8_t val;
 } cute_Bool;
+
 
 typedef struct 
 {
@@ -26,32 +40,93 @@ typedef struct
 } cute_Char;
 
 
+typedef struct 
+{
+    uint8_t val;
+} cute_Byte;
+
+
 // cute_Int
-inline cute_Int
-cute_Int__create__0(int64_t i);
 
 inline cute_Int
-cute_Int__create__1(cute_Float f);
+cute_Int__create__0(int64_t c);
+
+inline cute_Int
+cute_Int__create__1(cute_Int c);
+
+inline cute_Int
+cute_Int__create__2(cute_UInt c);
+
+inline cute_Int
+cute_Int__create__3(cute_Float c);
+
+inline cute_Int
+cute_Int__create__4(cute_Bool c);
+
+inline cute_Int
+cute_Int__create__5(cute_Byte c);
 
 inline int64_t
-cute_Int__val(cute_Int self);
+cute_Int__val(cute_Int i);
+
+
+
+// cute_UInt
+
+inline cute_UInt
+cute_UInt__create__0(uint64_t c);
+
+inline cute_UInt
+cute_UInt__create__1(cute_UInt c);
+
+inline cute_UInt
+cute_UInt__create__2(cute_Int c);
+
+inline uint64_t
+cute_UInt__val(cute_UInt i);
 
 
 // cute_Float
-inline cute_Float
-cute_Float__create__0(double d);
 
 inline cute_Float
-cute_Float__create__1(cute_Int i);
+cute_Float__create__0(double c);
+
+inline cute_Float
+cute_Float__create__1(cute_Float c);
+
+inline cute_Float
+cute_Float__create__2(cute_Int c);
 
 inline double
-cute_Float__val(cute_Float self);
+cute_Float__val(cute_Float f);
 
 
 // cute_Bool
-inline cute_Bool
-cute_Bool__create__0(cute_Int i);
 
-// cute_Char
-inline cute_Char
-cute_Char__create__0(cute_Int i);
+inline cute_Bool
+cute_Bool__create__0(uint8_t c);
+
+inline cute_Bool
+cute_Bool__create__1(cute_Bool c);
+
+inline cute_Bool
+cute_Bool__create__2(cute_Int c);
+
+inline cute_Bool
+cute_Bool__create__3(cute_UInt c);
+
+inline cute_Bool
+cute_Bool__create__4(cute_Byte c);
+
+inline uint64_t
+cute_Bool__val(cute_Bool b);
+
+
+// cute_Byte
+
+inline cute_Byte
+cute_Byte__create__0(uint8_t c);
+
+inline cute_Byte
+cute_Byte__create__1(cute_Byte c);
+
--- Cute/src/type.c
+++ Cute/src/type.c
@@ -1,54 +1,156 @@
 #include "CuteTypes.h"
 
 
+// cute_Int
+
 inline cute_Int
-cute_Int__create__0(int64_t i)
+cute_Int__create__0(int64_t c)
 {
-    return (cute_Int) {.val = i};
+    return (cute_Int) {.val = c};
 }
 
 inline cute_Int
-cute_Int__create__1(cute_Float f)
+cute_Int__create__1(cute_Int c)
 {
-    return (cute_Int) {.val = (int64_t) f.val};
+    return (cute_Int) {.val = c.val};
+}
+
+inline cute_Int
+cute_Int__create__2(cute_UInt c)
+{
+    return (cute_Int) {.val = (int64_t) c.val};
+}
+
+inline cute_Int
+cute_Int__create__3(cute_Float c)
+{
+    return (cute_Int) {.val = (int64_t) c.val};
+}
+
+inline cute_Int
+cute_Int__create__4(cute_Bool c)
+{
+    return (cute_Int) {.val = c.val ? 1 : 0};
+}
+
+inline cute_Int
+cute_Int__create__5(cute_Byte c)
+{
+    return (cute_Int) {.val = c.val};
 }
 
 inline int64_t
-cute_Int__val(cute_Int self)
+cute_Int__val(cute_Int i)
 {
-    return self.val;
+    return i.val;
 }
 
 
-inline cute_Float
-cute_Float__create__0(double d)
+
+// cute_UInt
+
+inline cute_UInt
+cute_UInt__create__0(uint64_t c)
 {
-    return (cute_Float) {.val = d};
+    return (cute_UInt) {.val = c};
+}
+
+inline cute_UInt
+cute_UInt__create__1(cute_UInt c)
+{
+    return (cute_UInt) {.val = c.val};
+}
+
+inline cute_UInt
+cute_UInt__create__2(cute_Int c)
+{
+    return (cute_UInt) {.val = (uint64_t) c.val};
+}
+
+inline uint64_t
+cute_UInt__val(cute_UInt i)
+{
+    return i.val;
+}
+
+
+// cute_Float
+
+inline cute_Float
+cute_Float__create__0(double c)
+{
+    return (cute_Float) {.val = c};
 }
 
 inline cute_Float
-cute_Float__create__1(cute_Int i)
+cute_Float__create__1(cute_Float c)
 {
-    return (cute_Float) {.val = (double) i.val};
+    return (cute_Float) {.val = c.val};
+}
+
+inline cute_Float
+cute_Float__create__2(cute_Int c)
+{
+    return (cute_Float) {.val = (double) c.val};
 }
 
 inline double
-cute_Float__val(cute_Float self)
+cute_Float__val(cute_Float f)
 {
-    return self.val;
+    return f.val;
 }
 
 
 // cute_Bool
+
 inline cute_Bool
-cute_Bool__create__0(cute_Int i)
+cute_Bool__create__0(uint8_t c)
 {
-    return (cute_Bool) {.val = i.val};
+    return (cute_Bool) {.val = c};
+}
+
+inline cute_Bool
+cute_Bool__create__1(cute_Bool c)
+{
+    return (cute_Bool) {.val = c.val};
+}
+
+inline cute_Bool
+cute_Bool__create__2(cute_Int c)
+{
+    return (cute_Bool) {.val = c.val == 0 ? 0 : 1};
+}
+
+inline cute_Bool
+cute_Bool__create__3(cute_UInt c)
+{
+    return (cute_Bool) {.val = c.val == 0 ? 0 : 1};
+}
+
+inline cute_Bool
+cute_Bool__create__4(cute_Byte c)
+{
+    return (cute_Bool) {.val = c.val == 0 ? 0 : 1};
+}
+
+inline uint64_t
+cute_Bool__val(cute_Bool b)
+{
+    return b.val;
 }
 
 
-inline cute_Char
-cute_Char__create__0(cute_Int i)
+// cute_Byte
+
+inline cute_Byte
+cute_Byte__create__0(uint8_t c)
 {
-    return (cute_Char) {.val = i.val};
+    return (cute_Byte) {.val = c};
 }
+
+inline cute_Byte
+cute_Byte__create__1(cute_Byte c)
+{
+    return (cute_Byte) {.val = c.val};
+}
+
